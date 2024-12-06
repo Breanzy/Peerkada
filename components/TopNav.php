5040c38b951d91d725c59e3ac1717b899bea375b
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-12-07 03:15:57 +1100

    fix Top Navbar Alignment & Logo

--- components/TopNav.php
+++ components/TopNav.php
@@ -1,18 +1,16 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark justify-content-between">
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark p-2">
     <!-- Navbar Brand-->
-
-    <div class="col-xl-6 col-6">
-        <div class="row justify-content-start">
-            <div class="col-auto"><a class="m-0 fw-bold fs-3 text-start m-3 navbar-brand" href="index.php">PEERKADA</a></div>
-            <!-- Sidebar Toggle-->
-            <div class="col-auto text-start"><button class="btn btn-link btn-sm" id="sidebarToggle" href="#!"><i class="fas fa-bars">SUP</i></button></div>
+    <div class="d-flex align-items-center">
+        <div class="">
+            <img src="../assets/logowhite.png" alt="logo" class="" style="max-height: 75px;"> <!-- Use img-fluid for responsiveness -->
         </div>
+        <a class="m-0 fw-bold fs-3 text-white navbar-brand text-center" href="index.php">PEERKADA</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm ms-3" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     </div>
 
-    <div class="col-xl-3 col-6">
-        <div class="row justify-content-end">
-            <div class="col-auto text-white text-end fw-lighter">Logged in as: <?php echo (isset($_SESSION["name"]) ? $_SESSION["name"] : "Guest"); ?> </div>
-            <div class="col-auto"><a class="text-white text-end fw-bold m-3" href="../controllers/logout.php">Log Out</a></div>
-        </div>
+    <div class="ms-auto d-flex align-items-center">
+        <div class="text-white fw-lighter">Logged in as: <?php echo (isset($_SESSION["name"]) ? $_SESSION["name"] : "Guest"); ?></div>
+        <a class="text-white fw-bold ms-3" href="../controllers/logout.php">Log Out</a>
     </div>
 </nav>
