31c38857a474ce60f115462cc5d3d8936b2c0d2e
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-02-14 15:30:31 +1100

    Added Conditions for Nav button access

--- components/SideNav.php
+++ components/SideNav.php
@@ -4,27 +4,31 @@
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Core</div>
                 <a class="nav-link" href="../pages/index.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                    QR Code
-                </a>
-                <a class="nav-link" href="../pages/index.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                     Dashboard
                 </a>
 
-                <div class="sb-sidenav-menu-heading">User</div>
-                <a class="nav-link" href="../pages/Profile.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                    Profile
-                </a>
+                <?php if (isset($_SESSION['name']) && $_SESSION['name'] == 'ADMIN') { ?>
+                    <a class="nav-link" href="../pages/AdminDash.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                        Admin Dashboard
+                    </a>
+                <?php } ?>
+
+
+                <?php if (isset($_SESSION['name'])) { ?>
+                    <div class="sb-sidenav-menu-heading">User</div>
+                    <a class="nav-link" href="../pages/Profile.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                        Profile
+                    </a>
+                <?php } ?>
+
                 <a class="nav-link" href="register.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                     Create New Profile
                 </a>
-                <a class="nav-link" href="../pages/AdminDash.php">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                    Admin Dashboard
-                </a>
+
             </div>
         </div>
     </nav>
