75374ee75d302093be0023305a04274fd2cfa02e
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-02-16 23:35:40 +1100

    Added AttendanceTable as component

--- pages/AdminDash.php
+++ pages/AdminDash.php
@@ -37,74 +37,22 @@ if (!isset($_SESSION['isAdmin'])) {
                     ?>
 
                     <h2>Attendance Logs (Signed Out)</h2>
-                    <table id="attendanceTable" class="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Student ID</th>
-                                <th>Time In</th>
-                                <th>Time Out</th>
-                                <th>Logdate</th>
-                                <th>Status</th>
-                                <th>Actions</th> <!-- New column for actions -->
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if (!empty($attendanceLogs)) {
-                                foreach ($attendanceLogs as $log) { ?>
-                                    <tr>
-                                        <td><?php echo htmlspecialchars($log['STUDENTID']); ?></td>
-                                        <td><?php echo date('h:i:s A', (strtotime($log['TIMEIN']))); ?></td>
-                                        <td><?php echo date('h:i:s A', (strtotime($log['TIMEOUT']))) ?></td>
-                                        <td><?php echo htmlspecialchars($log['LOGDATE']); ?></td>
-                                        <td><?php echo htmlspecialchars($log['STATUS']); ?></td>
-                                        <td>
-                                            <button type="button" onclick="editRow(this)">Edit</button>
-                                            <button type="button" onclick="deleteRow(this)">Delete</button>
-                                        </td>
-                                    </tr>
-                                <?php }
-                            } else { ?>
-                                <tr>
-                                    <td colspan="6">No attendance logs found.</td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                    <?php
+                    // Fetch attendance logs where TIMEOUT is not null
+                    $tableID = 'signedOut';
+                    $query = "SELECT * FROM table_attendance WHERE TIMEOUT IS NOT NULL";
+                    require '../components/TableAttendance.php';
+                    ?>
 
                     <h2>Attendance Logs (Missing Sign Out)</h2>
-                    <table id="missingSignOutTable" class="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Student ID</th>
-                                <th>Time In</th>
-                                <th>Time Out</th>
-                                <th>Logdate</th>
-                                <th>Status</th>
-                                <th>Actions</th> <!-- New column for actions -->
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if (!empty($missingSignOutLogs)) {
-                                foreach ($missingSignOutLogs as $log) { ?>
-                                    <tr>
-                                        <td><?php echo htmlspecialchars($log['STUDENTID']); ?></td>
-                                        <td><?php echo htmlspecialchars($log['TIMEIN']); ?></td>
-                                        <td><?php echo htmlspecialchars("Missing"); ?></td>
-                                        <td><?php echo htmlspecialchars($log['LOGDATE']); ?></td>
-                                        <td><?php echo htmlspecialchars($log['STATUS']); ?></td>
-                                        <td>
-                                            <button type="button" onclick="editRow(this)">Edit</button>
-                                            <button type="button" onclick="deleteRow(this)">Delete</button>
-                                        </td>
-                                    </tr>
-                                <?php }
-                            } else { ?>
-                                <tr>
-                                    <td colspan="5">No missing sign out logs found.</td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                    <?php
+                    // Fetch attendance logs where TIMEOUT is null
+                    $tableID = 'notSignedOut';
+                    $query = "SELECT * FROM table_attendance WHERE TIMEOUT IS NULL";
+                    require '../components/TableAttendance.php';
+
+                    ?>
+
                 </form>
             </main>
         </div>
